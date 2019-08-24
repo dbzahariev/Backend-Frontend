a2dfd603f7987ba5ade617542bdfe355cdc82cd2
Dimitar Zahariev <d.zahariev@devrealm.bg>
2019-08-24 16:51:02 +0300

    Add connect to DB

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-const express = require("express");
+const express = require('express');
+const connectDb = require('./config/db');
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("API Running");
+connectDb();
+
+app.get('/', (req, res) => {
+  res.send('API Running');
 });
 
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server start on port ${port}`));

3a16a143754274dd71b6d7e1509ff87f87019042
Dimitar Zahariev <d.zahariev@devrealm.bg>
2019-08-24 19:16:47 +0300

    Add midleware

---
+++ middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req, res, next) {
+  const token = req.header('x-auth-token');
+
+  // check if not token
+  if (!token) {
+    return res.status(401).json({
+      errors: {
+        msg:
+          'No token, authorization denied. (Add header with name: x-auth-token)'
+      }
+    });
+  }
+
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ errors: { msg: 'Token is not valid' } });
+  }
+};

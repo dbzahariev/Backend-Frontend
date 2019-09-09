d8acc6c05d0aae45f1497f1bb6448360ade4761e
Dimitar Zahariev <d.zahariev@devrealm.bg>
2019-09-09 13:37:39 +0300

    Add spinner

--- new-cours/backend-frontend-2019/src/App.js
+++ new-cours/backend-frontend-2019/src/App.js
@@ -2,14 +2,28 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/layouts/Navbar";
 import Users from "./components/users/Users";
+import axios from "axios";
 
 class App extends React.Component {
+  state = {
+    loading: false,
+    users: []
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
+    const { users, loading } = this.state;
+
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users users={users} loading={loading} />
         </div>
       </div>
     );
--- new-cours/backend-frontend-2019/src/components/users/Users.js
+++ new-cours/backend-frontend-2019/src/components/users/Users.js
@@ -1,40 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import UserItems from "./UserItems";
+import PropTypes from "prop-types";
+import Spinner from "../layouts/Spinner";
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: "1",
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github/mojombo"
-      },
-      {
-        id: "2",
-        login: "defunkt",
-        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-        html_url: "https://github/defunkt"
-      },
-      {
-        id: "3",
-        login: "pjhyett",
-        avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-        html_url: "https://github/pjhyett"
-      }
-    ]
-  };
-
-  render() {
-    return (
-      <div style={usersStyle}>
-        {this.state.users.map(user => (
-          <UserItems key={user.id} user={user} />
-        ))}
-      </div>
-    );
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
   }
-}
+  return (
+    <div style={usersStyle}>
+      {users.map(user => (
+        <UserItems key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+};
 
 const usersStyle = {
   display: "grid",

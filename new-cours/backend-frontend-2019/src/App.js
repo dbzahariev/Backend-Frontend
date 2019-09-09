5916bf41a3bac1d6052232ff79b82bc3894184d6
Dimitar Zahariev <d.zahariev@devrealm.bg>
2019-09-09 11:54:28 +0300

    Add navbar

--- new-cours/backend-frontend-2019/src/App.js
+++ new-cours/backend-frontend-2019/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/layouts/Navbar";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <Navbar />
       </div>
     );
   }

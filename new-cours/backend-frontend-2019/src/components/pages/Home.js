0f7b4f1b6256a2d508402ae0e6eb39ecd4ac9aeb
Dimitar Zahariev <d.zahariev@devrealm.bg>
2019-09-10 13:20:38 +0300

    Test

---
+++ new-cours/backend-frontend-2019/src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Search from "../../components/users/Search";
+import Users from "../../components/users/Users";
+
+const Home = props => {
+  return (
+    <React.Fragment>
+      <p>Test</p>
+      <Search />
+      <Users />
+    </React.Fragment>
+  );
+};
+
+export default Home;
